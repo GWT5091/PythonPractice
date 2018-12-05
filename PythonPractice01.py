eed98373490e013486f95b519fd978d54fea49ce
GWT5091 <32433807+GWT5091@users.noreply.github.ac.jp>
2018-12-06 00:45:08 +0900

    FirstConnit(I want to survive)

---
+++ PythonPractice01.py
@@ -0,0 +1,12 @@
+#Python_Practice01(12/6/thu)
+#Input-Output
+print('Hello world!')
+print('What is your name?')
+my_name = input()
+print('It is good of your name')
+
+#Run
+# Hello world!
+# What is your name?
+# GWT5091
+# It is good of your name
