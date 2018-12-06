78840e3057ff354b6f6775fc7ee0409bec610831
GWT5091 <32433807+GWT5091@users.noreply.github.ac.jp>
2018-12-07 00:10:53 +0900

    退屈なことはPythonにやらせよう１〜３章のコードを自分なりに改編した

---
+++ PythonPractice02_1.py
@@ -0,0 +1,11 @@
+#Python_Practice02(12/7/thu)
+#Input-Output
+#I use first time "from" Sentence
+from random import *
+for i in range(5):
+    print(randint(1,10))
+
+#Same
+# import random
+# for i in range(5):
+#     print(random.randint(1,10))
